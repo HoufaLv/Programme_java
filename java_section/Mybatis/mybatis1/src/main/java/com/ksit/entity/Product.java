08c9cd0923e70906dfd802607b5ed518575db533
HoufaLv <HoufaLv@outlook.com>
2018-03-19 22:33:38 +0800

    尝试使用mybats框架,完成配置文件编辑

---
+++ java_section/Mybatis/mybatis1/src/main/java/com/ksit/entity/Product.java
@@ -0,0 +1,32 @@
+package com.ksit.entity;
+
+public class Product {
+
+    private Integer id;
+    private String productName;
+    private String productPrice;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+}
