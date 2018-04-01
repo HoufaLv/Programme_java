85e9b6c174140f8a9babb9c2b32db0de79c9d160
HoufaLv <HoufaLv@outlook.com>
2018-04-01 20:58:42 +0800

    演示 jdk动态代理模式

--- java_section/javaseTest/src/test/java/com/ksit/proxy/Mp3AdProxyTest.java
+++ java_section/javaseTest/src/test/java/com/ksit/proxy/Mp3AdProxyTest.java
@@ -2,6 +2,10 @@ package com.ksit.proxy;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 import static org.junit.Assert.*;
 
 public class Mp3AdProxyTest {
@@ -13,7 +17,40 @@ public class Mp3AdProxyTest {
         Mp3AdProxy mp3AdProxy = new Mp3AdProxy(mp3);
         mp3AdProxy.play("广告代理类播放音乐");
         mp3AdProxy.stop();
-        
+
+    }
+
+    @Test
+    public void testJdkProxy(){
+        //创建目标对象
+        final Mp3 mp3 = new Mp3();
+        //获得目标对象的类加载器
+        ClassLoader classLoader = mp3.getClass().getClassLoader();
+        //获得目标对象的接口数组
+        Class<?>[] interfaces = mp3.getClass().getInterfaces();
+
+        Player player = (Player) Proxy.newProxyInstance(classLoader, interfaces, new InvocationHandler() {
+            /**
+             * @param proxy     正在执行的代理对象
+             * @param method    客户调用的方法(目标对象的方法)
+             * @param args      方法的参数列表
+             * @return 方法的返回值
+             * @throws Throwable
+             */
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                String methodName = method.getName();
+                System.out.println("方法" +methodName+ "开始调用");
+                Object result = method.invoke(mp3, args);
+                System.out.println("方法" +methodName+ "调用结束");
+
+                return result;
+            }
+        });
+
+
+        player.play("歌曲");
+        player.stop();
     }
 
 }
